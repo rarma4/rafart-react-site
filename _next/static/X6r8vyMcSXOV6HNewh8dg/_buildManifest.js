2b13926f4399f5dc3b16f5cfc0d03e3bf1eaf7e1
rarma4 <rarma4@gmail.com>
2022-10-25 01:38:35 +0000

    Deploying to gh-pages from @ rarma4/rafart-react-site@5c3d3941aac6f6881daad1cc174c97d454db214a 🚀

--- _next/static/X6r8vyMcSXOV6HNewh8dg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/375-cd27b79b4c8d7ea8.js","static/chunks/pages/index-71af4437b624d413.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
